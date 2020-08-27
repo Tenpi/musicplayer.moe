a903617f1cccc56c764b50ab6ab89cde36e2b40b
Tenpi <37512637+Tenpi@users.noreply.github.com>
2020-08-26 21:10:06 -0400

    worker

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+import * as serviceWorker from "./public/service-worker"
 
 ReactDOM.render(<App/>, document.getElementById("app"))
 
 if (process.env.TESTING === "yes") {
     serviceWorker.unregister()
 } else {
-    serviceWorker.register()
+    serviceWorker.register("/service-worker.js")
 }
