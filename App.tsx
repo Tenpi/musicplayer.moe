4294e2e53aacade276104176e671b198a9c4674c
Tenpi <37512637+Tenpi@users.noreply.github.com>
2020-08-29 18:52:30 -0400

    typo

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from "react"
 import {Switch, Route} from "react-router-dom"
 import HomePage from "./components/HomePage"
 import $404 from "./components/404"
-import functions from "./structures/functions"
+import functions from "./structures/Functions"
 
 const App: React.FunctionComponent = () => {
   useEffect(() => {
